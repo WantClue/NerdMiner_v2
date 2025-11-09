fcb7d10cba40ef7aa3f149bc99e362d03fbca51e
WantClue <wantclue@users.noreply.github.com>
2025-11-09 10:02:22 +0100

    default pool

--- src/drivers/storage/storage.h
+++ src/drivers/storage/storage.h
@@ -12,10 +12,10 @@
 #define DEFAULT_SSID		"HanSoloAP"
 #endif
 #define DEFAULT_WIFIPW		"MineYourCoins"
-#define DEFAULT_POOLURL		"public-pool.io"
+#define DEFAULT_POOLURL		"pool.nerdminer.de"
 #define DEFAULT_POOLPASS	"x"
 #define DEFAULT_WALLETID	"yourBtcAddress"
-#define DEFAULT_POOLPORT	21496
+#define DEFAULT_POOLPORT	3333
 #define DEFAULT_TIMEZONE	2
 #define DEFAULT_SAVESTATS	false
 #define DEFAULT_INVERTCOLORS	false
